f75e2f827bf86f909b55ece9aa8bc6d65ddbfb09
NguyenKhacTung <tungbgx004@gmail.com>
2024-11-03 16:46:58 +0700

    ok

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.example.demo.Config;
-
 import com.example.demo.Service.CustomAuthenticationSuccessHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
