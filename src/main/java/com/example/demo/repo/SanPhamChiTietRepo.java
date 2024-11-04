00aebd4843e1e7c1928a4e6b903ebd23aaaca713
Lê git config --global user.name Lê <lam090142@gmail.com>
2024-11-04 15:18:23 +0700

    fix

--- src/main/java/com/example/demo/repo/SanPhamChiTietRepo.java
+++ src/main/java/com/example/demo/repo/SanPhamChiTietRepo.java
@@ -2,28 +2,24 @@ package com.example.demo.repo;
 
 import com.example.demo.entity.SanPham;
 import com.example.demo.entity.SanPhamChiTiet;
-import org.hibernate.mapping.List;
+//import org.hibernate.mapping.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-<<<<<<< HEAD
-=======
+
 import org.springframework.data.repository.query.Param;
->>>>>>> feature/sanpham
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-<<<<<<< HEAD
-public interface SanPhamChiTietRepo extends JpaRepository<SanPhamChiTiet,Integer> {
-    @Query("SELECT spct FROM SanPhamChiTiet spct JOIN spct.idSanPham sp WHERE LOWER(sp.ten) LIKE LOWER(CONCAT('%', :ten, '%'))")
-    List<SanPhamChiTiet> findBySanPhamTenContainingIgnoreCase(String ten);
-}
-=======
+
 public interface SanPhamChiTietRepo extends JpaRepository<SanPhamChiTiet, Integer> {
 
+    @Query("SELECT spct FROM SanPhamChiTiet spct JOIN spct.idSanPham sp WHERE LOWER(sp.ten) LIKE LOWER(CONCAT('%', :ten, '%'))")
+    List<SanPhamChiTiet> findBySanPhamTenContainingIgnoreCase(String ten);
 
     @Query("SELECT s FROM SanPhamChiTiet s WHERE s.idSanPham.idSanPham = :idSanPham")
     Page<SanPhamChiTiet> getByID(@Param("idSanPham") Integer idSanPham, Pageable pageable);
@@ -32,4 +28,4 @@ public interface SanPhamChiTietRepo extends JpaRepository<SanPhamChiTiet, Intege
 
 
 }
->>>>>>> feature/sanpham
+
