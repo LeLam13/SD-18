1814213c7dd28dd221e9e446c934a8c5c71680f8
Lê git config --global user.name Lê <lam090142@gmail.com>
2024-11-05 21:59:45 +0700

    fix

--- src/main/java/com/example/demo/repo/khachhangRePo.java
+++ src/main/java/com/example/demo/repo/khachhangRePo.java
@@ -1,18 +1,14 @@
 package com.example.demo.repo;
 
 import com.example.demo.entity.khachhang;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface KhachHangRepo extends JpaRepository<khachhang, Integer> {
-    @Query("SELECT k FROM khachhang k WHERE k.soDienThoai = :soDienThoai")
-    List<khachhang> findBySoDienThoai(String soDienThoai);
+
 }
